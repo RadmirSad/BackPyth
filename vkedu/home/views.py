6fe7d097d9f448d8e0704af29699de85deed6235
RadmirSad <62589656+RadmirSad@users.noreply.github.com>
2021-11-09 17:22:45 +0300

    Add files via upload

---
+++ vkedu/home/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def landing_page(request):
+    if request.method == 'GET':
+        return HttpResponse('This is a main page')
+    else:
+        return HttpResponse('This method doesn\'t support', status=501)
