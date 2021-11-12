6a66987c4114866dbe9f921e4470986584dad54a
RadmirSad <62589656+RadmirSad@users.noreply.github.com>
2021-11-12 19:45:33 +0300

    Add files via upload

--- vkedu/home/views.py
+++ vkedu/home/views.py
@@ -7,4 +7,4 @@ def landing_page(request):
     if request.method == 'GET':
         return HttpResponse('This is a main page')
     else:
-        return HttpResponse('This method doesn\'t support', status=501)
+        return HttpResponse('This method doesn\'t support', status=405)
