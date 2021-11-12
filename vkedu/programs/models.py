8d086b05bbf96d68fba17f8da2cd487995af463e
RadmirSad <62589656+RadmirSad@users.noreply.github.com>
2021-11-12 19:46:15 +0300

    Add files via upload

--- vkedu/programs/models.py
+++ vkedu/programs/models.py
@@ -5,7 +5,7 @@ from schedule.models import Lesson
 
 
 class Program(models.Model):
-    NameOfProgram = models.CharField(verbose_name='Название курса', max_length=30, null=False)
-    Lessons = models.ForeignKey(Lesson, verbose_name='Список вебинаров', blank=False,
+    name = models.CharField(verbose_name='Название курса', max_length=30, null=False)
+    lessons = models.ForeignKey(Lesson, verbose_name='Список вебинаров', blank=False,
                                 null=False, on_delete=models.PROTECT)
 
--- vkedu/programs/views.py
+++ vkedu/programs/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http.response import JsonResponse, HttpResponse
+from django.http.response import JsonResponse
 
 # Create your views here.
 
 
 def prog_name(request, my_id=0):
     if request.method == 'GET':
-        return JsonResponse({'Name': 'Backend'})
+        return JsonResponse({'Name' + str(my_id): 'Backend'})
     elif request.method == 'POST':
-        return JsonResponse('Updated successfully', safe=False)
+        return JsonResponse({'Updated successfully': my_id})
     else:
-        return HttpResponse('This method doesn\'t support', status=501)
+        return JsonResponse({request.method: 'Not supported'}, status=405)
